961942e9cab4905114ad44e8472409ecb9c4e658
Razry <54051601+Razry@users.noreply.github.com>
2021-03-08 10:48:58 +0300

    Исправления в java

--- Lesson_5/src/main/java/MyFirstProgram.java
+++ Lesson_5/src/main/java/MyFirstProgram.java
@@ -1,6 +1,7 @@
 public class MyFirstProgram {
 
-  public static void main(String[] args){
-System.out.println("hello world"); 
+  public static void main (String [] args){
+
+    System.out.println("hello world");
 }
 }
