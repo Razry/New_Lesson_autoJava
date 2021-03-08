e3b537963d4574cf87b7a076d90d9ef4447d8ee6
Nik <kolyaarxipov@mail.ru>
2021-03-08 15:35:29 +0300

    Добавление подсчета площади прямоугольника

--- Lesson_5/src/main/java/MyFirstProgram.java
+++ Lesson_5/src/main/java/MyFirstProgram.java
@@ -2,20 +2,29 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
 
-      Hello("Alex");
-      Hello("Nikolay");
-      Hello("Denis");
+      hello("Alex");
+      hello("Nikolay");
+      hello("Denis");
 
-      double len = 5;
+      double len = 5; //Для квадрата
         System.out.println("Площадь квардрата со стороной " + len + " * "+ len + " = " + area(len));
+
+        double a = 5;
+        double b = 6;
+        System.out.println("Площадь квардрата со стороной " + a + " и " + b + " = " + prem(a,b));
     }
 
-    public static void Hello(String somebody) {
+    public static void hello(String somebody) {
         System.out.println("hello world " + somebody + "! ");
     }
 
     public static double area(double l){
         return l*l;
+    }
 
+    public static double prem(double a, double b){
+        double S;
+        S = a * b;
+        return S;
     }
 }
